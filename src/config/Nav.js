fcef380954a6b994bcb9e234942b02a058f3fbf8
Gahlen Fridley <gahlen.fridley@gmail.com>
2019-09-27 11:07:55 -0600

    Change to Navbar

--- src/config/Nav.js
+++ src/config/Nav.js
@@ -4,15 +4,19 @@ import "../styles/Nav.css";
 
 const Nav = ({ login }) => {
   return (
-    <nav className="navbar">
+    <nav className="navbar" >
+      <span>
       <Link className= "left-align" to = "/home">Home</Link>
       <Link className= "left-align" to = "/ledger">Ledger</Link>
       <Link className= "left-align" to = "/category">Category Management</Link>
       <Link className= "left-align" to = "/budgetSummary">Budget Summary</Link>
       <Link className= "left-align" to = "/budgetManage">Budget Management</Link>
       <Link className= "left-align" to = "/report">Reports</Link>
+      </span>
+      <span>
       <Link className= "right-align" to = "/auth">Register</Link> 
       <Link className= "right-align" to = "/login">Login</Link>
+      </span>
     </nav>
   );
 };
